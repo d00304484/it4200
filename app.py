07f2a2251fb19466831ef9fe34377365e2713f6c
Yomi <aodubiyi1@gmail.com>
2024-09-24 11:27:07 -0600

    Added GitHub Actions workflows

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+"""
+This module contains a simple Flask app that returns 'Hello World!'
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """Route to return 'Hello World!'"""
     return "Hello World!"
 
 if __name__ == '__main__':
--- test/test_app.py
+++ test/test_app.py
@@ -1,9 +1,13 @@
+"""
+This module contains unit tests for the Flask app.
+"""
+
+from app import app
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app
-
 def test_hello():
+    """Test the '/' route to ensure it returns 'Hello World!'"""
     response = app.test_client().get('/')
     assert response.data == b"Hello World!"
